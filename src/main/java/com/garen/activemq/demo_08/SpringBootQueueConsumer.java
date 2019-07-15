ff907f2615da5791c0525464001a7101a207bcec
leibing.song <315439102@qq.com>
2019-07-15 15:58:12 +0800

    spring boot整合activemq

--- src/main/java/com/garen/activemq/demo_08/SpringBootQueueConsumer.java
+++ src/main/java/com/garen/activemq/demo_08/SpringBootQueueConsumer.java
@@ -1,7 +1,16 @@
 package com.garen.activemq.demo_08;
 
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
 
 @Component
 public class SpringBootQueueConsumer {
+
+    @JmsListener(destination = "spring-boot-queue")
+    public void consumer(TextMessage message) throws JMSException {
+        System.out.println(message.getText());
+    }
 }
